2309f6fa0a2905953bc044fa83661ae06d2ecf81
Conrad Storz <conradstorz@gmail.com>
2025-03-14 00:12:03 -0400

    changed definition of config file location

--- config.py
+++ config.py
@@ -3,7 +3,11 @@
 import os
 from configparser import ConfigParser
 
-configfile = os.environ["VENDINGMACHINE_CONFIG_FILE"]  # Raises error if not set
+# Construct the full path to the config file in the current working directory
+configfile = os.path.join(os.getcwd(), "VENDINGMACHINE_CONFIG_FILE")
+
+if not os.path.exists(configfile):
+    raise FileNotFoundError(f"Configuration file not found: {configfile}")
 
 config = ConfigParser()
 config.read(configfile, encoding="utf-8")
