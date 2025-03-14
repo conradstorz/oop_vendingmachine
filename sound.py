3adc4f0bdc697eaca1ff1f91d748347c6e9b6cf0
Conrad Storz <conradstorz@gmail.com>
2025-03-14 00:06:20 -0400

    commented out all functionality for now

--- sound.py
+++ sound.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import random
-from mpyg321.mpyg321 import MPyg321Player
-from pkg_resources import resource_listdir, resource_filename
+# from mpyg321.mpyg321 import MPyg321Player
+# from pkg_resources import resource_listdir, resource_filename
 
-PLAYER = MPyg321Player()
+# PLAYER = MPyg321Player()
 
 PACKAGE = "vendingmachine.resources.sounds"
 
@@ -20,16 +20,17 @@ def get_random_mp3_file(subpackage):
     """Example: get_random_mp3_file(COIN_REJECT)"""
     mp3s = []
     subpackage = "{}.{}".format(PACKAGE, subpackage)
-    for n in resource_listdir(subpackage, ""):
-        filename = resource_filename(subpackage, n)
-        if filename.endswith(".mp3"):
-            mp3s.append(filename)
+    # for n in resource_listdir(subpackage, ""):
+    #    filename = resource_filename(subpackage, n)
+    #    if filename.endswith(".mp3"):
+    #        mp3s.append(filename)
     return random.choice(mp3s)
 
 
 def play_random(subpackage):
-    PLAYER.play_song(get_random_mp3_file(subpackage))
-
+    # PLAYER.play_song(get_random_mp3_file(subpackage))
+    pass
 
 def stop():
-    PLAYER.stop()
+    # PLAYER.stop()
+    pass
